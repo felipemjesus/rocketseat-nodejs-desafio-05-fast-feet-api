1c940b0dca1230d0abb16420c78f0996446f636b
Felipe Martins <felipemjesus@outlook.com>
2025-03-05 10:11:50 -0300

    Adicionado o core domain

---
+++ src/core/entities/entity.ts
@@ -0,0 +1,25 @@
+import { UniqueEntityId } from './unique-entity-id'
+
+export class Entity<Props> {
+  private _id: UniqueEntityId
+  protected props: Props
+
+  protected constructor(props: Props, id?: UniqueEntityId) {
+    this.props = props
+    this._id = id ?? new UniqueEntityId()
+  }
+
+  get id() {
+    return this._id
+  }
+
+  public equals(entity: Entity<unknown>) {
+    if (entity === this) {
+      return true
+    }
+    if (entity.id === this.id) {
+      return true
+    }
+    return false
+  }
+}
